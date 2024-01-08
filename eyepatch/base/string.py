8d3a3e50f2da8ce76104dce2cff320bb9394cf8f
m1stadev <adamhamdi31@gmail.com>
2024-01-08 13:27:40 -0600

    base.string: update - disasm -> patcher - patcher is an optional arg - update imports - add replace function for patching string

--- eyepatch/base/string.py
+++ eyepatch/base/string.py
@@ -1,15 +1,18 @@
+from typing import Optional, Union
+
+import eyepatch.base
+
+
 class _ByteString:
-    def __init__(self, disasm: '_Disassembler', data: bytes, offset: int):  # noqa: F821
-        self._disasm = disasm
-        self._data = data
+    def __init__(
+        self, offset: int, data: bytes, patcher: Optional[eyepatch.base._Patcher] = None
+    ):
         self._offset = offset
+        self._data = data
+        self._patcher = patcher
 
     def __repr__(self) -> str:
-        return f'0x{self.offset:x}: {self.string}'
-
-    @property
-    def disasm(self) -> '_Disassembler':  # noqa: F821
-        return self._disasm
+        return f'0x{self.offset:x}: "{self.string}"'
 
     @property
     def data(self) -> bytes:
@@ -22,3 +25,26 @@ class _ByteString:
     @property
     def offset(self) -> int:
         return self._offset
+
+    @property
+    def patcher(self) -> Optional[eyepatch.base._Patcher]:
+        return self._patcher
+
+    def replace(
+        self,
+        oldvalue: Union[str, bytes],
+        newvalue: Union[str, bytes],
+        count: Optional[int],
+    ) -> None:
+        if isinstance(oldvalue, str):
+            oldvalue = oldvalue.encode()
+
+        if isinstance(newvalue, str):
+            newvalue = newvalue.encode()
+
+        if oldvalue not in self._data:
+            # TODO: raise error
+            pass
+
+        self._data = self._data.replace(oldvalue, newvalue, count)
+        self.patcher._data[self.offset : self.offset + len(self._data)] = self._data
