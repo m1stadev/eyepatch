0a72aa2aa486b33bb9f3551ad88c81b25a7af045
m1stadev <adamhamdi31@gmail.com>
2024-01-08 19:21:07 -0600

    aarch64.xref: use last operand for `xref()`

--- eyepatch/aarch64/xref.py
+++ eyepatch/aarch64/xref.py
@@ -8,9 +8,9 @@ import eyepatch.aarch64
 class _XrefMixin:
     def xref(self, skip: int = 0) -> Optional[eyepatch.aarch64.Insn]:  # noqa: F821
         for insn in self.patcher.disasm(0x0):
-            for op in insn.data.operands:
-                if op.type == ARM64_OP_IMM and (op.imm + insn.offset) == self.offset:
-                    if skip == 0:
-                        return insn
+            op = insn.data.operands[-1]
+            if op.type == ARM64_OP_IMM and (op.imm + insn.offset) == self.offset:
+                if skip == 0:
+                    return insn
 
-                    skip -= 1
+                skip -= 1
