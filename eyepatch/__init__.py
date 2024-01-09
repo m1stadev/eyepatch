e99a81c26070947a2336a504005868f94ab3ff5b
m1stadev <adamhamdi31@gmail.com>
2024-01-08 21:45:35 -0600

    init: import aarch64 patcher as AArch64Patcher and arm patcher as ARMPatcher

--- eyepatch/__init__.py
+++ eyepatch/__init__.py
@@ -1,3 +1,6 @@
 from importlib.metadata import version
 
+from .aarch64 import Patcher as AArch64Patcher  # noqa: F401
+from .arm import Patcher as ARMPatcher  # noqa: F401
+
 __version__ = version(__package__)
