4e500ac21504a0e81084d0ec15d84766569b5618
m1stadev <adamhamdi31@gmail.com>
2024-01-11 01:38:16 -0600

    aarch64: xref fix not every instruction has operands

--- eyepatch/aarch64.py
+++ eyepatch/aarch64.py
@@ -28,6 +28,9 @@ else:
 class _XrefMixin:
     def xref(self, skip: int = 0) -> Optional['Insn']:  # noqa: F821
         for insn in self.patcher.disasm(0x0):
+            if len(insn.info.operands) == 0:
+                continue
+
             op = insn.info.operands[-1]
             if op.type == ARM64_OP_IMM and (op.imm + insn.offset) == self.offset:
                 if skip == 0:
