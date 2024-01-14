30a0da991f417c5bb31a8ad6f2bddfd89aa03315
m1stadev <adamhamdi31@gmail.com>
2024-01-13 18:19:17 -0600

    reqs: add logger

--- eyepatch/__init__.py
+++ eyepatch/__init__.py
@@ -1,6 +1,11 @@
 from importlib.metadata import version
 
+from loguru import logger as _logger
+
 from .aarch64 import Patcher as AArch64Patcher  # noqa: F401
 from .arm import Patcher as ARMPatcher  # noqa: F401
+from .errors import *  # noqa: F403
 
 __version__ = version(__package__)
+
+_logger.disable('eyepatch')
